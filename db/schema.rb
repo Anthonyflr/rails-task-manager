1110b192548222a68745595fcb2b50d09824734b
Anthony Fleury <anthony.fleury@outlook.com>
2017-02-14 11:11:53 -0200

    Added description to model

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170214125154) do
+ActiveRecord::Schema.define(version: 20170214130255) do
 
   create_table "tasks", force: :cascade do |t|
     t.string   "name"
     t.string   "status"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+    t.string   "description"
   end
 
 end
